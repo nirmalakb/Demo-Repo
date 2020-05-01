3927d0e677cb71bdc7b103eef79d8cc292c51728
unknown <knirmalabiradar@gmail.com>
2020-05-01 23:43:48 +0530

    first git commit for selenium code

---
+++ src/main/java/com/qa/pages/HomePage.java
@@ -0,0 +1,10 @@
+package com.qa.pages;
+
+public class HomePage {
+	
+	public void home(){
+		
+		System.out.println("Home page");
+	}
+
+}
---
+++ src/main/java/com/qa/util/TestUtils.java
@@ -0,0 +1,9 @@
+package com.qa.util;
+
+public class TestUtils {
+
+	
+	public void util(){
+		System.out.println("util method");
+	}
+}
